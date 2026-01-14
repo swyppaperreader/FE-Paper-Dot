dab438c164a83449950db04dff4d0e096918b7de
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-01-14 23:27:39 +0900

    [Feat] 프로젝트 기본 세팅(전역 상태 라이브러리(zustand)설치, color 토큰 생성, 폰트설정(roboto))

---
+++ app/color/color.ts
@@ -0,0 +1,42 @@
+export const colors = {
+  primary: {
+    500: "#558AF0",
+    700: "#1D4084",
+  },
+
+  label: {
+    900: "#21232A",
+    800: "#474952",
+    700: "#4B4D55",
+    600: "#575D64",
+    500: "#737A82",
+    400: "#8F959D",
+    300: "#B7B9C0",
+    200: "#C7CDD4",
+    100: "#DEE3EB",
+    50: "#F0F4FA",
+  },
+
+  interaction: {
+    default: "#191F28",
+    disabled: "#B0B8C1",
+  },
+
+  background: {
+    primary: "#FFFFFF",
+    secondary: "#F2F4F6",
+  },
+
+  divider: {
+    default: "#E1E1E1",
+  },
+
+  status: {
+    error: "#FF4E4E",
+  },
+
+  base: {
+    white: "#FFFFFF",
+    black: "#222222",
+  },
+};
