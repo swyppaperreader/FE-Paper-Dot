14311770d389659367f78986fd890b03f445cb40
hjkl <goni1653@hufs.ac.kr>
2026-01-31 22:49:32 +0900

    [Style] 로고 이미지 반영

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+// Footer.tsx
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./Footer.module.css";
@@ -9,7 +10,7 @@ export default function Footer() {
         {/* 1줄: 로고 */}
         <div className={styles.logoSection}>
           <Image
-            src="/Paperdot.logo.png"
+            src="/paperdotlogo-M.png"
             alt="Paperdot 로고"
             width={212}
             height={80}
@@ -20,25 +21,22 @@ export default function Footer() {
 
         {/* 2줄: 링크 | 저작권 | 이메일 */}
         <div className={styles.contentSection}>
-          {/* 왼쪽: 개인정보처리방침 | 이용약관 */}
           <div className={styles.linkGroup}>
             <Link href="/privacy" className={styles.link}>
               개인정보처리방침
             </Link>
-            <span className={styles.divider}>·</span>
+            <span className={styles.divider}>|</span>
             <Link href="/terms" className={styles.link}>
               이용약관
             </Link>
           </div>
 
-          {/* 중앙: Copyright */}
           <div className={styles.copyrightSection}>
             <p className={styles.copyright}>
-              © 2026 paperdot. All rights reserved.
+              © {new Date().getFullYear()} paperdot. All rights reserved.
             </p>
           </div>
 
-          {/* 오른쪽: 문의 이메일 */}
           <div className={styles.emailSection}>
             <a href="mailto:olola9791@gmail.com" className={styles.email}>
               문의: olola9791@gmail.com
