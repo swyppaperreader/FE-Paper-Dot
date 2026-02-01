f6a3ff88129cad109c52c6f889e3d98a2cb1bd49
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-01 18:16:02 +0900

    [Feat] turbPack에서 webpack으로 수정

--- app/components/layout/main/SecondMainScreen.tsx
+++ app/components/layout/main/SecondMainScreen.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Button from "@/app/components/button/Button";
 import styles from "./secondMainScreen.module.css";
 import SecondMainScreenImage from "@/public/backgroundSecondImage.svg";
