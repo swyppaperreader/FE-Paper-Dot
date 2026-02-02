01ec122ef32d7431ffd3664562a1742ccc231900
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-02 16:32:17 +0900

    [Chore] 컴포넌트 대문자로 변경

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./header.module.css";
 import HeaderLogo from "@/public/Logo.svg";
-import IsLoginHeader from "./isLogin/isLoginHeader";
+import IsLoginHeader from "./isLogin/IsLoginHeader";
 
 export default function Header() {
   const isLogin = false;
--- app/components/header/isLogin/isLoginHeader.tsx
+++ app/components/header/isLogin/isLoginHeader.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-export default function isLoginHeader() {
+export default function IsLoginHeader() {
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
