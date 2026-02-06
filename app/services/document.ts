a24a0f16e7282bcc6b4a22748afa3e78edb91975
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-06 14:16:49 +0900

    [Feat] Documents api 수정

--- app/services/document.ts
+++ app/services/document.ts
@@ -1,14 +1,17 @@
 export const postDocuments = async (formData: FormData) => {
   try {
-    const response = await fetch("https://be-paper-dot.store/api/documents", {
+    const response = await fetch("https://be-paper-dot.store/documents", {
       method: "POST",
       body: formData,
     });
+
+    console.log("response", response);
 
     if (!response.ok) {
       throw new Error("파일 업로드에 실패했습니다!");
     }
     const data = await response.json();
+    console.log("data", data);
     return data;
   } catch (error) {
     throw new Error((error as Error).message || "파일 업로드에 실패했습니다!");
