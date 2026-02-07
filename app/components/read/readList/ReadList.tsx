bca5420967b748d992435333d9f97f616f44a940
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-08 00:00:55 +0900

    [Feat] 파일네임 데이터 나오도록 수정

--- app/components/read/readList/ReadList.tsx
+++ app/components/read/readList/ReadList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReadHeader from "../../header/ReadHeader";
 
 interface TranslationPair {
@@ -10,8 +10,21 @@ interface TranslationPair {
 }
 
 export default function ReadList() {
-  const data = JSON.parse(sessionStorage.getItem("translationPairs") ?? "[]");
-  const fileName = JSON.parse(sessionStorage.getItem("fileName") ?? "");
+  const [data, setData] = useState<TranslationPair[]>([]);
+  const [fileName, setFileName] = useState<string>("");
+
+  useEffect(() => {
+    const storedPairs = sessionStorage.getItem("translationPairs");
+    const storedFileName = sessionStorage.getItem("fileName");
+
+    if (storedPairs) {
+      setData(JSON.parse(storedPairs));
+    }
+
+    if (storedFileName) {
+      setFileName(JSON.parse(storedFileName));
+    }
+  }, []);
 
   console.log(data);
   console.log(fileName);
