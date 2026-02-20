53364bb1c81e32c931f980cbd26a04e19f3c554e
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-20 18:17:10 +0900

    [Feat] a 태그 link 태그로 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,9 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./login.module.css";
+import Link from "next/link";
 
 export default function LoginPage() {
-  // const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-  // const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-
   const handleKakaoLogin = () => {
     window.location.href = `https://be-paper-dot.store/oauth2/authorization/kakao`;
   };
@@ -40,20 +38,20 @@ export default function LoginPage() {
           <p className={styles.termsText}>
             로그인 시 이용약관 및 개인정보처리방침에 동의하게 됩니다.
           </p>
-          <a
+          <Link
             href="https://www.notion.so/2fbeb2f40de780c5b996fc19312ca446?source=copy_link"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.termsLinkText}>
             이용약관
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.notion.so/2f4eb2f40de7802f8539e4762234b41d?source=copy_link"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.termsLinkText}>
             개인정보처리방침
-          </a>
+          </Link>
         </div>
       </div>
     </div>
