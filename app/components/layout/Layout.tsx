1fc331bfc2c7d0bfefbc4edccac4c2a88c8da418
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-03-08 13:34:13 +0900

    [Feat] 특정경로 접근 못하도록 수정

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -4,10 +4,23 @@ import React from "react";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import Sidebar from "@/app/mypage/sidebar/page";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useLoginStore } from "@/app/store/useLogin";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const userInfo = useLoginStore((state) => state.userInfo);
+  const router = useRouter();
+
+  const protectedRoutes =
+    pathname.includes("/mypage") || pathname.includes("/read");
+
+  if (protectedRoutes && !userInfo) {
+    toast.error("로그인 상태가 아닙니다. 다시 로그인해주세요.");
+    router.push("/login");
+    return <ToastContainer autoClose={1000} />;
+  }
 
   const isMypage =
     pathname === "/mypage/mydocument" || pathname === "/mypage/account";
